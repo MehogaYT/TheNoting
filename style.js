1ba5b63cb76ecfed0139a46856737a24e71de4d3
MehogaYT <156496600+MehogaYT@users.noreply.github.com>
2025-05-30 16:03:04 +0800

    Update style.js

--- style.js
+++ style.js
@@ -1,12 +1,10 @@
-<h1 id="judul">Judul Berganti Warna</h1>
+const colors = ['red', 'blue', 'green', 'orange', 'purple'];
+let i = 0;
+const elemJudul = document.getElementById('judul');
+const elemCatatan = document.getElementById('catatan');
 
-<script>
-  const colors = ['red', 'blue', 'green', 'orange', 'purple'];
-  let i = 0;
-  const elem = document.getElementById('judul');
-
-  setInterval(() => {
-    elem.style.color = colors[i];
-    i = (i + 1) % colors.length;
-  }, 1000);
-</script>
+setInterval(() => {
+  elemJudul.style.color = colors[i];
+  elemCatatan.style.color = colors[(i + 2) % colors.length]; // warna beda untuk catatan
+  i = (i + 1) % colors.length;
+}, 1000);
